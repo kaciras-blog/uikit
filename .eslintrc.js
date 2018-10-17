c02aeaeb94b6c1d951adff6f96b65ac1f1da62bb
Kaciras <kaciras@outlook.com>
2018-10-17 18:10:38 +0800

    add source files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	"extends": [
+		"plugin:vue/essential",
+		"@vue/standard",
+	],
+	rules: {
+		"indent": ["error", "tab", { SwitchCase: 1 }],
+		"no-tabs": "off",
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"no-return-assign": "off",
+		"comma-dangle": ["error", "always-multiline"],
+		"no-mixed-spaces-and-tabs": ["warn", "smart-tabs"],
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+};
