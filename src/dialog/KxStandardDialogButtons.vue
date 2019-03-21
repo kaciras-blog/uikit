765958a6ef2df895f723182ab1c4125f846cfd25
Kaciras <kaciras@outlook.com>
2019-03-21 14:35:37 +0800

    temp KSDB

--- src/dialog/KxStandardDialogButtons.vue
+++ src/dialog/KxStandardDialogButtons.vue
@@ -1,4 +1,4 @@
-<template>
+<!--
 	<div class="btn-group" :class="$style.container">
 		<kx-button
 			v-if="cancelButton"
@@ -16,24 +16,32 @@
 			确定
 		</kx-button>
 	</div>
-</template>
+-->
 
 <script>
 export default {
 	name: "KxStandardDialogButtons",
+	functional: true,
 	props: {
 		cancelButton: Boolean,
 		applyHook: Function,
 		confirmHook: Function,
 	},
-	methods: {
-		confirm() {
-			if(this.confirmHook) {
-				this.confirmHook();
-			} else {
-				this.$dialog.confirm();
-			}
-		},
+	render (h, ctx) {
+		const { props, listeners } = ctx;
+		const buttons = [];
+
+		if (props.cancelButton) {
+			buttons.push(h("kx-button", { on: { click: this.$dialog.close } }, "取消"))
+		}
+		if (props.applyHook) {
+			buttons.push(h("kx-button", { on: { click: props.applyHook } }, "应用"))
+		}
+
+		const onConfirm = listeners.confirm || this.$dialog.confirm;
+		buttons.push(h("kx-button", { staticClass: "primary", on: { click: onConfirm } }, "确定"));
+
+		return h("div", { class: ["btn-group", this.$style.container] }, buttons)
 	},
 };
 </script>
