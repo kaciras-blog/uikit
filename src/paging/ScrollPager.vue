89545bc3d04210b5afde287a6a04cd2c9f9fc378
Kaciras <Kaciras@outlook.com>
2019-04-30 21:35:51 +0800

    ScrollPager: optimize style & remove listener after destroy

--- src/paging/ScrollPager.vue
+++ src/paging/ScrollPager.vue
@@ -16,9 +16,11 @@
 		<kx-button
 			v-if="state==='FREE'"
 			tag="a"
+			class="primary"
+			icon="fas fa-chevron-circle-down"
 			:href="nextPageUrl"
 			@click.prevent="loadPage">
-			下一页
+			查看更多
 		</kx-button>
 	</div>
 </template>
@@ -30,23 +32,24 @@ const FAILED = "FAILED";
 const ALL_LOADED = "ALL_LOADED";
 
 class LoadTask {
-	constructor (vm) {
+
+	constructor(vm) {
 		this._finish = false;
 		this._vm = vm;
 	}
 
-	complete (allLoaded = false) {
+	complete(allLoaded = false) {
 		// 可能一次加载后空余高度仍达不到activeHeight，还得继续加载
 		if (!this._vm.tryLoadPage()) {
 			this.finish(allLoaded ? ALL_LOADED : FREE);
 		}
 	}
 
-	completeWithError () {
+	completeWithError() {
 		this.finish(FAILED);
 	}
 
-	finish (state) {
+	finish(state) {
 		if (this._finish) {
 			throw new Error("不能重复设置加载的结果");
 		}
@@ -79,19 +82,19 @@ export default {
 			default: FREE,
 		},
 	},
-	data () {
+	data() {
 		/* free,fail,loading,allLoaded */
 		return { state: this.initState };
 	},
 	computed: {
-		loadable () {
+		loadable() {
 			return [ALL_LOADED, LOADING].indexOf(this.state) < 0;
 		},
 	},
 	methods: {
-		tryLoadPage () {
-			const { state, activeHeight } = this;
-			if (state !== FREE) {
+		tryLoadPage() {
+			const { state, activeHeight, autoLoad } = this;
+			if (state !== FREE && !autoLoad) {
 				return;
 			}
 			// 网页高度 - 窗口高度 - 窗口之上部分的高度 = 窗口下面剩余的高度
@@ -102,17 +105,18 @@ export default {
 			}
 			return remain < activeHeight;
 		},
-		loadPage () {
+		loadPage() {
 			if (!this.loadable) return;
 			this.state = LOADING;
 			this.$emit("load-page", new LoadTask(this));
 		},
 	},
-	mounted () {
-		if (this.autoLoad) {
-			window.addEventListener("scroll", this.tryLoadPage);
-		}
+	mounted() {
+		window.addEventListener("scroll", this.tryLoadPage);
 		this.tryLoadPage();
+	},
+	destroyed() {
+		window.removeEventListener("scroll", this.tryLoadPage);
 	},
 };
 </script>
