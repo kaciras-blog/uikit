c5970dd88f63bfd68e5269e6136b8393452f6de4
Kaciras <Kaciras@outlook.com>
2019-05-25 21:23:23 +0800

    storybook: fix extensions

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,5 +4,9 @@ module.exports = async ({ config, mode }) => {
 	const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd());
 	service.init(mode.toLowerCase());
 	const custom = service.resolveWebpackConfig();
-	return { ...config, module: { ...config.module, rules: custom.module.rules } };
+
+	config = { ...config, module: { ...config.module, rules: custom.module.rules }};
+	Object.assign(config.resolve.extensions, custom.resolve.extensions);
+
+	return config;
 };
--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,18 @@
 import { storiesOf } from "@storybook/vue";
 import KxButton from "../src/components/KxButton.vue";
+import KxTaskButton from "../src/components/KxTaskButton.vue";
+import { sleep } from "../src/index";
 
-storiesOf("Button", module)
-	.add("as a component", () => ({
-		components: { KxButton },
-		template: "<kx-button>rounded</kx-button>",
-	}));
+
+storiesOf("Button", module).add("plain button", () => ({
+	components: { KxButton },
+	template: "<kx-button>rounded</kx-button>",
+})).add("task button", () => ({
+	components: { KxTaskButton },
+	template: "<kx-task-button :on-click='load'>加载按钮</kx-task-button>",
+	methods: {
+		load() {
+			return sleep(3000);
+		},
+	},
+}));
