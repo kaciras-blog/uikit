99672fb9c7836f809c3babc7be3df4bd4870f122
Kaciras <Kaciras@outlook.com>
2019-05-30 14:06:23 +0800

    add $style to shims-vue

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,11 @@
-declare module '*.vue' {
-	import Vue from 'vue'
-	export default Vue
+import Vue from "vue";
+
+declare module "vue/types/vue" {
+	interface Vue {
+		$style: { [key: string]: string };
+	}
+}
+
+declare module "*.vue" {
+	export default Vue;
 }
