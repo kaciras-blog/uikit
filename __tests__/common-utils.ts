bd24c756119b019a42819dc07ecbdd908cf6cb99
Kaciras <Kaciras@outlook.com>
2019-06-15 19:09:13 +0800

    fix throttleFirst `this` arg passing

--- __tests__/common-utils.ts
+++ __tests__/common-utils.ts
@@ -1,24 +1,38 @@
 import { throttleFirst } from "../src/common";
 import { PromiseCompletionSource } from "../src/PromiseDelegate";
 
-test("throttleFirst avoid multiple calls", (done) => {
-	let task!: PromiseCompletionSource<number>;
-	const func = (a: number, b: number) => task = new PromiseCompletionSource();
-	const throttled = throttleFirst(func);
+describe("throttleFirst", () => {
 
-	expect(throttled(5, 6)).toBe(throttled(3, 4));
+	it("should pass context", async () => {
+		const obj = {
+			value: 333,
+			func() { return Promise.resolve(this.value); },
+		};
 
-	task.resolve(123);
+		obj.func = throttleFirst(obj.func);
+		expect(await obj.func()).toBe(333);
+	});
 
-	throttled(1, 2)
-		.then(v => expect(v).toBe(123))
-		.then(done);
+	it("should avoid multiple calls", (done) => {
+		let task!: PromiseCompletionSource<number>;
+		const func = (a: number, b: number) => task = new PromiseCompletionSource();
+		const throttled = throttleFirst(func);
+
+		expect(throttled(5, 6)).toBe(throttled(3, 4));
+
+		task.resolve(123);
+
+		throttled(1, 2)
+			.then(v => expect(v).toBe(123))
+			.then(done);
+	});
+
+	it("should call after first resolved", async () => {
+		const func = (x: number) => Promise.resolve(x * x);
+		const throttled = throttleFirst(func);
+
+		await throttled(3).then(v => expect(v).toBe(9));
+		await throttled(4).then(v => expect(v).toBe(16));
+	});
 });
 
-test("throttleFirst call after first resolved", async () => {
-	const func = (x: number) => Promise.resolve(x * x);
-	const throttled = throttleFirst(func);
-
-	await throttled(3).then(v => expect(v).toBe(9));
-	await throttled(4).then(v => expect(v).toBe(16));
-});
