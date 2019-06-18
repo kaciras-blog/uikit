ad422097f6c57d50f80562795d3c4c87771daf2e
Kaciras <Kaciras@outlook.com>
2019-06-18 23:14:22 +0800

    add exception test for debounceFirst

--- __tests__/common-utils.ts
+++ __tests__/common-utils.ts
@@ -16,23 +16,39 @@ describe("debounceFirst", () => {
 	it("should avoid multiple calls", (done) => {
 		let task!: PromiseCompletionSource<number>;
 		const func = (a: number, b: number) => task = new PromiseCompletionSource();
-		const throttled = debounceFirst(func);
+		const debounced = debounceFirst(func);
 
-		expect(throttled(5, 6)).toBe(throttled(3, 4));
+		expect(debounced(5, 6)).toBe(debounced(3, 4));
 
 		task.resolve(123);
 
-		throttled(1, 2)
+		debounced(1, 2)
 			.then(v => expect(v).toBe(123))
 			.then(done);
 	});
 
 	it("should call after first resolved", async () => {
 		const func = (x: number) => Promise.resolve(x * x);
-		const throttled = debounceFirst(func);
+		const debounced = debounceFirst(func);
 
-		await throttled(3).then(v => expect(v).toBe(9));
-		await throttled(4).then(v => expect(v).toBe(16));
+		await debounced(3).then(v => expect(v).toBe(9));
+		await debounced(4).then(v => expect(v).toBe(16));
+	});
+
+	it("should reset after exception", async (done) => {
+		const debounced = debounceFirst(async (throws) => {
+			if (throws) {
+				throw new Error();
+			}
+			return 123456;
+		});
+		try {
+			await debounced(true);
+			done.fail("expect to throws");
+		} catch (e) {
+			expect(await debounced(false)).toBe(123456);
+			done();
+		}
 	});
 });
 
