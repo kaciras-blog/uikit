5c9b4430ecf72ac02b8eb9fa251122d9df7b6910
Kaciras <Kaciras@outlook.com>
2019-10-31 16:24:36 +0800

    intro KxSwitch.vue

---
+++ src/components/KxSwitch.vue
@@ -0,0 +1,97 @@
+<template>
+	<div
+		class="kx-switch"
+		:class="{ checked: value, disabled: disabled }"
+		role="switch"
+		:aria-checked="value"
+		:aria-disabled="disabled"
+		@click.prevent="switchValue"
+	>
+		<!--suppress HtmlFormInputWithoutLabel -->
+		<input
+			:name="name"
+			:id="id"
+			class="kx-switch-input"
+			type="checkbox"
+			@change="handleChange"
+			:disabled="disabled"
+		>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "KxSwitch",
+	props: {
+		value: {
+			type: Boolean,
+			default: false,
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		id: String,
+		name: String,
+	},
+	methods: {
+		switchValue() {
+			if (this.disabled) {
+				return;
+			}
+			this.$emit("input", !this.value);
+		},
+		handleChange(event) {
+			this.$emit("input", event.target.value);
+		}
+	}
+}
+</script>
+
+<style lang="less">
+@import "../css/exports";
+
+@checked-color: #2196F3;
+
+.kx-switch {
+	display: inline-block;
+	width: 40px;
+	height: 20px;
+	padding: 2px;
+
+	border-radius: 8px;
+	cursor: pointer;
+	background: gray;
+
+	transition: .2s ease-out;
+
+	&::after {
+		content: "";
+		display: inline-block;
+		width: 16px;
+		height: 100%;
+
+		background: white;
+		border-radius: 8px;
+		transition: .2s ease-out;
+	}
+
+	&.checked {
+		background: @checked-color;
+	}
+
+	&.checked::after {
+		margin-left: 20px;
+	}
+
+	&.disabled {
+		cursor: default;
+		background: lightgray;
+	}
+}
+
+.kx-switch-input {
+	position: absolute;
+	opacity: 0;
+}
+</style>
