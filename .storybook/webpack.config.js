93a94345c7ea40ce46848aaba19ec9dbf7ff7b11
Kaciras <Kaciras@outlook.com>
2019-11-03 18:16:26 +0800

    fix storybook build config `resolve.alias`

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,13 @@
-const Service = require('@vue/cli-service/lib/Service');
+const Service = require("@vue/cli-service/lib/Service");
 
 module.exports = async ({ config, mode }) => {
 	const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd());
 	service.init(mode.toLowerCase());
 	const custom = service.resolveWebpackConfig();
 
-	config = { ...config, module: { ...config.module, rules: custom.module.rules }};
+	config = { ...config, module: { ...config.module, rules: custom.module.rules } };
 	Object.assign(config.resolve.extensions, custom.resolve.extensions);
 
+	config.resolve.alias = { ...custom.resolve.alias, ...config.resolve.alias };
 	return config;
 };
