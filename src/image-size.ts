ec88ead874f0dac61c44dea62d6b496efb962c70
Kaciras <Kaciras@outlook.com>
2020-05-27 17:26:27 +0800

    image-size.ts: use viewBox when `width` | `height` attr has context-aware unit

--- src/image-size.ts
+++ src/image-size.ts
@@ -1,7 +1,7 @@
 type Size = { width: number, height: number };
 
 /**
- * 获取图片的尺寸，该函数基于 HTMLImageElement 只能在浏览器端使用。
+ * 获取像素图的尺寸，该函数只能在浏览器端使用。
  *
  * @param image 图片文件或URL
  * @return 尺寸 { width, height }，单位像素
@@ -24,12 +24,18 @@ export function getRasterImageSize(image: string | Blob) {
 }
 
 /**
+ * 获取SVG图片的尺寸，该函数只能在浏览器端使用。
+ *
  * 有些SVG没有设置width和height属性，比如AI导出时选择了“响应”选项，
  * 此时无法通过<img>来获取尺寸，应选用SVG的viewBox属性。
  *
+ * 【注意】
+ * SVG的长宽值可能含有小数，在转换为JS Number时可能产生精度误差。
+ *
  * @param image 图片文件或URL
+ * @return 尺寸 { width, height }，单位像素
  */
-export async function getSVGImageSize(image: string | Blob) {
+export async function getSVGImageSize(image: string | Blob): Promise<Size> {
 	let svgText;
 	if (typeof image === "string") {
 		svgText = await (await fetch(image)).text();
@@ -41,11 +47,27 @@ export async function getSVGImageSize(image: string | Blob) {
 	const doc = parser.parseFromString(svgText, "image/svg+xml");
 	const svg = doc.documentElement as unknown as SVGSVGElement;
 
-	const width = svg.width.baseVal;
-	const height = svg.height.baseVal;
+	/**
+	 * 仅支持px单位和不带单位，因为其它单位与上下文有关，
+	 *
+	 * @param length SVG长度
+	 * @throws 如果是不支持的单位
+	 */
+	function getLengthPx(length: SVGAnimatedLength) {
+		const { baseVal } = length;
 
-	if (width.unitType === 2 || height.unitType === 2) {
-		return svg.viewBox.baseVal;
+		switch (baseVal.unitType) {
+			case SVGLength.SVG_LENGTHTYPE_NUMBER:
+			case SVGLength.SVG_LENGTHTYPE_PX:
+				return baseVal.value;
+		}
+		throw new Error("Unsupported SVG length unit.");
 	}
-	return { width: width.value, height: height.value };
+
+	try {
+		return { width: getLengthPx(svg.width), height: getLengthPx(svg.height) };
+	} catch (e) {
+		const viewBox = svg.viewBox.baseVal;
+		return { width: viewBox.width, height: viewBox.height };
+	}
 }
--- stories/helper-functions.stories.js
+++ stories/helper-functions.stories.js
@@ -17,28 +17,43 @@ stories.add("CheckBox", () => ({
 			<kx-button @click="showImageFileSize">选择文件</kx-button>
 			<input v-model="url" placeholder="图片URL">
 			<kx-button @click="showImageURLSize">获取大小</kx-button>
+
 			<div v-if="size">width: {{size.width}}, height: {{size.height}}</div>
+			<img v-if="image" :src="image" alt="preview" :style="imgStyle">
 		</div>`,
 	data: () => ({
 		url: "",
 		size: null,
+		image: null,
 	}),
+	computed: {
+		imgStyle() {
+			return { width: this.size.width + "px", height: this.size.height + "px" };
+		},
+	},
 	methods: {
 		async showImageURLSize() {
-			const url = new URL(this.url);
-			if (url.pathname.endsWith("svg")) {
-				this.size = await getSVGImageSize(this.url);
+			const { url } = this;
+
+			if (new URL(url).pathname.endsWith("svg")) {
+				this.size = await getSVGImageSize(url);
 			} else {
-				this.size = await getRasterImageSize(this.url);
+				this.size = await getRasterImageSize(url);
 			}
+
+			this.image = url;
 		},
 		async showImageFileSize() {
 			const file = await openFile("image/*");
+
 			if (file.type.indexOf("svg") > -1) {
 				this.size = await getSVGImageSize(file);
 			} else {
 				this.size = await getRasterImageSize(file);
 			}
+
+			URL.revokeObjectURL(this.image);
+			this.image = URL.createObjectURL(file);
 		},
 	},
 }));
