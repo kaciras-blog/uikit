05aa7fcdc6b30ec2a57f558a5a28de9aa27d9e08
Kaciras <Kaciras@outlook.com>
2020-07-12 09:53:01 +0800

    polish

--- src/interactive.ts
+++ src/interactive.ts
@@ -5,6 +5,8 @@ export function openFile(accept: string, multiple: true): Promise<FileList>;
 /**
  * 弹出文件选择框，在用户点确定之后resolve。
  *
+ * TODO: 取消事件有点难办
+ *
  * @param accept 文件类型
  * @param multiple 是否多选，如果为true返回文件列表，否则返回单个文件
  * @return 一个Promise，将在用户点击确定时完成
@@ -17,7 +19,7 @@ export function openFile(accept: string, multiple = false) {
 	input.click();
 
 	return new Promise<File | FileList>((resolve) => {
-		input.addEventListener("change", (event) => {
+		input.addEventListener("change", event => {
 			// @ts-ignore
 			const files = event.target.files;
 			resolve(multiple ? files : files[0]);
