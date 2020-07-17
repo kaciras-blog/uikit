ea501323becbbfa2eb81c608bba9a3a6e1f14e52
Kaciras <Kaciras@outlook.com>
2020-07-17 17:00:29 +0800

    add story `lineClamp`

--- stories/others.stories.js
+++ stories/others.stories.js
@@ -11,5 +11,22 @@ export const tagGroup = () => ({
 		</div>
 	`,
 });
-
 tagGroup.story = { name: "TagGroup" };
+
+
+const defaultText = `
+Proprietary and undocumented CSS property that will contain text to a
+given amount of lines when used in combination with display: -webkit-box.
+It will end with ellipsis when text-overflow: ellipsis is included.`;
+
+export const lineClamp = () => ({
+	props: {
+		text: {
+			default: defaultText,
+		},
+	},
+	template: `
+		<div class="line-clamp"><article>{{text}}</article></div>
+	`,
+});
+lineClamp.story = { name: "LineClamp" };
