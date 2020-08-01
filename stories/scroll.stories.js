59fdcfd40d4802509b4d11a204492ac0e7f40a5b
Kaciras <Kaciras@outlook.com>
2020-08-01 15:35:44 +0800

    add a story for scrollToElementStart & scrollToElementEnd

--- stories/scroll.stories.js
+++ stories/scroll.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from "@storybook/vue";
-import { syncScroll } from "../src";
-import { boolean, withKnobs } from "@storybook/addon-knobs";
+import { scrollToElementEnd, scrollToElementStart, syncScroll } from "../src";
+import { boolean, button, withKnobs } from "@storybook/addon-knobs";
 
 const stories = storiesOf("Scroll", module);
 stories.addDecorator(withKnobs);
@@ -8,14 +8,15 @@ stories.addDecorator(withKnobs);
 stories.add("syncScroll", () => ({
 	props: {
 		enable: {
-			default: boolean("EnableSyncScroll", true),
+			default: boolean("Enabled", true),
 		},
 	},
 	template: `
 		<div class="sync-scroll">
 			<textarea ref="a" :value="content"/>
 			<textarea ref="b" :value="content"/>
-		</div>`,
+		</div>
+	`,
 	data() {
 		let content = "";
 		for (let i = 0; i < 100; i++) {
@@ -34,5 +35,23 @@ stories.add("syncScroll", () => ({
 	},
 	mounted() {
 		this.$_destroy = syncScroll(this.$refs.a, this.$refs.b);
+	},
+}));
+
+stories.add("ScrollTo", () => ({
+	template: `
+		<div style="height: 200vh"><div ref="target" class="scroll-to-box"/></div>
+	`,
+	methods: {
+		scrollToStart() {
+			scrollToElementStart(this.$refs.target);
+		},
+		scrollToEnd() {
+			scrollToElementEnd(this.$refs.target);
+		},
+	},
+	created() {
+		button("ScrollToStart", this.scrollToStart);
+		button("ScrollToEnd", this.scrollToEnd);
 	},
 }));
