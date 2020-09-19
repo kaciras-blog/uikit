e1a092e0047b9a26f97459ff371c93e94bf24d54
Kaciras <Kaciras@outlook.com>
2020-09-19 20:04:22 +0800

    fix storybook build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,20 @@
+const { join } = require("path");
 const Service = require("@vue/cli-service/lib/Service");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = async ({ config, mode }) => {
-	const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd());
+	const service = new Service(process.cwd());
 	service.init(mode.toLowerCase());
 	const custom = service.resolveWebpackConfig();
 
-	config = { ...config, module: { ...config.module, rules: custom.module.rules } };
+	const lessLoader = custom.module.rules.find(loader => loader.test.source === /\.less$/.source);
+	config.module.rules.push(lessLoader);
+
+	config.resolve.alias["@"] = join(__dirname, "..", "src");
+
 	Object.assign(config.resolve.extensions, custom.resolve.extensions);
 
-	config.resolve.alias = { ...custom.resolve.alias, ...config.resolve.alias };
-
-	if(mode === "PRODUCTION") {
+	if (mode === "PRODUCTION") {
 		config.plugins.push(new MiniCssExtractPlugin({
 			filename: "css/[name].[contenthash:8].css",
 			chunkFilename: "css/[name].[contenthash:8].css",
