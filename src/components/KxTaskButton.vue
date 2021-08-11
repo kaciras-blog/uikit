0042c4c65525f1929820aca2775650a8039a9b63
Kaciras <Kaciras@protonmail.com>
2021-08-11 20:31:07 +0800

    add abort support for KxTaskButton

--- src/components/KxTaskButton.vue
+++ src/components/KxTaskButton.vue
@@ -4,44 +4,48 @@
 		@click="handleClick"
 	>
 		<slot v-if="!running"/>
-		<slot v-else name="running"><slot/></slot>
+		<slot v-else name="running">
+			<slot/>
+		</slot>
 	</kx-button>
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref } from "vue";
+import { defineProps, ref, withDefaults } from "vue";
 
-// TODO: abort support
-
-const { waiting, onClick } = defineProps({
-
-	// 在运行状态下是否屏蔽点击事件
-	waiting: {
-		type: Boolean,
-		default: true,
-	},
+interface Props {
 
 	// 因为事件无法获取返回值所以用 props
-	onClick: {
-		type: Function,
-		required: true,
-	},
+	onClick: (event: Event, signal: AbortSignal) => Promise<any>;
+
+	// 在运行状态下是否屏蔽点击事件
+	waiting?: boolean;
+}
+
+const { waiting, onClick } = withDefaults(defineProps<Props>(), {
+	waiting: true,
 });
 
 const running = ref(false);
+const controller = ref(new AbortController());
 
 function handleClick(event: MouseEvent) {
-	if (running.value && waiting) {
-		return;
-	}
-	const task = onClick!(event);
+	if (running.value) {
+		if (!waiting) {
+			running.value = false;
+			controller.value.abort();
+		}
+	} else {
+		const { signal } = controller.value = new AbortController();
+		const task = onClick(event, signal);
 
-	if (typeof task.then !== "function") {
-		throw new Error("Click handler must return a Promise");
-	}
+		if (typeof task.finally !== "function") {
+			throw new Error("Click handler must return a Promise");
+		}
 
-	running.value = true;
-	task.finally(() => running.value = false);
+		running.value = true;
+		task.finally(() => running.value = false);
+	}
 }
 </script>
 
@@ -60,15 +64,13 @@ function handleClick(event: MouseEvent) {
 		background-size: @stripeWidth @stripeWidth;
 	}
 
-	background-image: linear-gradient(
-		-45deg,
-		var(--bg-highlight) 25%,
-		transparent 25%,
-		transparent 50%,
-		var(--bg-highlight) 50%,
-		var(--bg-highlight) 75%,
-		transparent 75%
-	);
+	background-image: linear-gradient(-45deg,
+	var(--bg-highlight) 25%,
+	transparent 25%,
+	transparent 50%,
+	var(--bg-highlight) 50%,
+	var(--bg-highlight) 75%,
+	transparent 75%);
 
 	animation: barbershop linear .4s infinite;
 }
--- stories/buttons.stories.js
+++ stories/buttons.stories.js
@@ -4,92 +4,62 @@ import { sleep } from "../src/index";
 // link buttons?
 export default {
 	title: "Button",
-};
-
-export const plainButtons = () => ({
-	template: `
-		<div>
-			<p>
-				<kx-button icon="far fa-paper-plane" @click="handleClick">默认样式</kx-button>
-			</p>
-			<p class="btn-group">
-				<kx-button class="primary">主要按钮</kx-button>
-				<kx-button class="second">次要按钮</kx-button>
-				<kx-button class="info">信息按钮</kx-button>
-				<kx-button class="dangerous">危险操作</kx-button>
-			</p>
-			<p class="btn-group">
-				<kx-button class="primary outline">主要按钮</kx-button>
-				<kx-button class="second outline">次要按钮</kx-button>
-				<kx-button class="info outline">信息按钮</kx-button>
-				<kx-button class="dangerous outline">危险操作</kx-button>
-			</p>
-		</div>
-	`,
-	methods: {
-		handleClick: action("button-click"),
-	},
-});
-
-plainButtons.storyName = "Plain";
-
-export const taskButtons = () => ({
-	template: `
-		<div>
-			<p class="btn-group">
-				<kx-task-button :on-click="load">
-					加载按钮
-					<template v-slot:running>正在加载</template>
-				</kx-task-button>
-				<kx-task-button class="second outline" :on-click="load">
-					加载按钮
-					<template v-slot:running>正在加载</template>
-				</kx-task-button>
-				<kx-task-button class="info outline" :on-click="load">
-					加载按钮
-					<template v-slot:running>正在加载</template>
-				</kx-task-button>
-				<kx-task-button class="dangerous" :on-click="load">
-					加载按钮
-					<template v-slot:running>正在加载</template>
-				</kx-task-button>
-			</p>
-		</div>`,
-	methods: {
-		load() {
-			return sleep(3000);
+	argTypes: {
+		waiting: {
+			control: { type: "boolean" },
 		},
 	},
-});
+};
 
-taskButtons.storyName = "Task";
-
-export const toolButtons = () => ({
+export const NormalButton = () => ({
 	template: `
-		<div class="toolbar">
-			<kx-tool-button class="tool-button" @click="handleClick">
-				<i class="far fa-question-circle"></i>
-			</kx-tool-button>
-			<kx-tool-button class="tool-button" @click="handleClick">
-				<i class="far fa-edit"></i>
-			</kx-tool-button>
-			<kx-tool-button class="tool-button" @click="handleClick">
-				<i class="fas fa-eye"></i>
-			</kx-tool-button>
-			<kx-tool-button class="tool-button" @click="handleClick">
-				<i class="far fa-trash-alt"></i>
-			</kx-tool-button>
-			<kx-tool-button class="tool-button" href="#" @click="handleClick">
-				<i class="far fa-paper-plane"></i>
-			</kx-tool-button>
-			<kx-tool-button class="tool-button" route="#" @click="handleClick">
-				Vue路由
-			</kx-tool-button>
-		</div>
+		<p>
+			<kx-button icon="far fa-paper-plane" @click="handleClick">默认样式</kx-button>
+		</p>
+		<p class="btn-group">
+			<kx-button class="primary">主要按钮</kx-button>
+			<kx-button class="second">次要按钮</kx-button>
+			<kx-button class="info">信息按钮</kx-button>
+			<kx-button class="dangerous">危险操作</kx-button>
+		</p>
+		<p class="btn-group">
+			<kx-button type="outline" class="primary outline">主要按钮</kx-button>
+			<kx-button type="outline" class="second outline">次要按钮</kx-button>
+			<kx-button type="outline" class="info outline">信息按钮</kx-button>
+			<kx-button type="outline" class="dangerous outline">危险操作</kx-button>
+		</p>
 	`,
 	methods: {
 		handleClick: action("button-click"),
 	},
 });
 
-toolButtons.storyName = "Tool";
+export const TaskButton = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	template: `
+		<div class="btn-group">
+			<kx-task-button :on-click="load">
+				加载按钮
+				<template v-slot:running>正在加载</template>
+			</kx-task-button>
+			<kx-task-button class="second outline" :on-click="load">
+				加载按钮
+				<template v-slot:running>正在加载</template>
+			</kx-task-button>
+			<kx-task-button class="info outline" :on-click="load">
+				加载按钮
+				<template v-slot:running>正在加载</template>
+			</kx-task-button>
+			<kx-task-button class="dangerous" :on-click="load">
+				加载按钮
+				<template v-slot:running>正在加载</template>
+			</kx-task-button>
+		</div>`,
+	methods: {
+		load: () => sleep(3000),
+	},
+});
+
+TaskButton.args = {
+	waiting: false,
+};
