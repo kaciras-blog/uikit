e0bb397cd818d5fda44bfd3fe2fdd7142d964b1a
Kaciras <Kaciras@protonmail.com>
2021-08-19 16:08:45 +0800

    fix storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,25 +1,39 @@
 const { join } = require("path");
 const Service = require("@vue/cli-service/lib/Service");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = async ({ config, mode }) => {
+/**
+ * 对 StoryBook 和 Vue-CLI 的整合分为两步：调整 Vue-CLI 的配置，然后合并。
+ * 本函数是第一步，负责初始化配置以及移除一些冲突的插件。
+ *
+ * @see // https://github.com/storybookjs/vue-cli-plugin-storybook/blob/master/lib/preset.js
+ */
+function getCliConfig(mode) {
 	const service = new Service(process.cwd());
 	service.init(mode.toLowerCase());
-	const custom = service.resolveWebpackConfig();
+	const chain = service.resolveChainableWebpackConfig();
+
+	const allowedPlugins = [
+		"friendly-errors",
+		"no-emit-on-errors",
+		"extract-css",
+		// "optimize-css",
+	];
+
+	chain.plugins.values().map((item) => item.name)
+		.filter(name => !allowedPlugins.includes(name))
+		.forEach(name => chain.plugins.delete(name));
+
+	return service.resolveWebpackConfig(chain);
+}
+
+module.exports = ({ config, mode }) => {
+	const custom = getCliConfig(mode);
 
 	const lessLoader = custom.module.rules.find(loader => loader.test.source === /\.less$/.source);
 	config.module.rules.push(lessLoader);
-
-	config.resolve.alias["@"] = join(__dirname, "..", "src");
-
+	config.resolve.alias["@"] = join(__dirname, "../src");
+	config.plugins.push(...custom.plugins);
 	Object.assign(config.resolve.extensions, custom.resolve.extensions);
-
-	if (mode === "PRODUCTION") {
-		config.plugins.push(new MiniCssExtractPlugin({
-			filename: "css/[name].[contenthash:8].css",
-			chunkFilename: "css/[name].[contenthash:8].css",
-		}));
-	}
 
 	// require("../scripts/dump-webpack-config")("storybook.js", config);
 	return config;
