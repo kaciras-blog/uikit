09c81b451e3eff5d86c68c6152e050f41b76a767
Kaciras <Kaciras@protonmail.com>
2021-08-26 19:02:34 +0800

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
 	root: true,
 	extends: [
-		"@kaciras/typescript",
 		"@kaciras/core",
+		"@kaciras/typescript",
 
 		// 已经设置了 vue-eslint-parser
 		"plugin:vue/vue3-essential",
@@ -16,11 +16,18 @@ module.exports = {
 	rules: {
 		"vue/require-explicit-emits": ["error", { allowProps: true }],
 		"vue/no-lone-template": ["error", { ignoreAccessible: true }],
+		"vue/no-template-shadow": "error",
+		"vue/custom-event-name-casing": "error",
 	},
 	overrides: [
 		{
 			files: ["**/__tests__/*.{j,t}s?(x)"],
 			extends: ["@kaciras/jest"],
+		},
+		{
+			files: ["*.vue"],
+			extends: ["plugin:@typescript-eslint/recommended"],
+			parser: "vue-eslint-parser",
 		},
 	],
 };
