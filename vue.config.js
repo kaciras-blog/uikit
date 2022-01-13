010fb6b41b581c1aae636c3715379df85bf68363
Kaciras <Kaciras@protonmail.com>
2022-01-13 18:27:01 +0800

    add build command & remove babel

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 const svgLoader = require.resolve("@kaciras-blog/devtool/lib/webpack/vue-svg-loader");
+const packageJson = require("./package.json");
 
 module.exports = {
 	lintOnSave: false,
 
 	chainWebpack: (config) => {
+		config.externals(Object.keys(packageJson.dependencies));
+
 		const svgRule = config.module.rule("svg");
-
 		svgRule.uses.clear();
-
 		svgRule
 			.use("vue-loader").loader("vue-loader")
 			.end()
