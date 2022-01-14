e1702cb45add593f46941df540d00a392a236401
Kaciras <Kaciras@protonmail.com>
2022-01-14 19:58:16 +0800

    fix SVG loader chain

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const svgLoader = require.resolve("@kaciras-blog/devtool/lib/webpack/reactive-sv
 const sfcLoader = require.resolve("@kaciras-blog/devtool/lib/webpack/vue-template-loader");
 const packageJson = require("./package.json");
 
+/**
+ * 自定义下 vue-cli 的 webpack，改动包括：
+ * 1）全部依赖外置，反正 website 项目里还是会打包，节省点编译时间。
+ * 2）将 SVG 加载为 Vue 组件。
+ */
 module.exports = {
 	lintOnSave: false,
 
@@ -11,6 +16,7 @@ module.exports = {
 		const svgRule = config.module.rule("svg");
 		svgRule.uses.clear();
 		svgRule
+			.delete("type").delete("generator")
 			.use("vue-loader").loader("vue-loader")
 			.end()
 			.use("sfc-loader").loader(sfcLoader)
