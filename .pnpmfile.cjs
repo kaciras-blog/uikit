396b7722114f694a8c77adb236f74ee212608431
Kaciras <Kaciras@protonmail.com>
2022-02-01 22:57:55 +0800

    avoid annoying react peerDependency warnings

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -5,6 +5,10 @@ function readPackage(packageJson) {
 	delete dependencies["@types/react"];
 	delete peerDependencies["@types/react"];
 
+	// storybook 把 react（也不需要）作为 peer 依赖，安装时搞出一堆烦人的警告。
+	delete peerDependencies["react"];
+	delete peerDependencies["react-dom"];
+
 	return packageJson;
 }
 
