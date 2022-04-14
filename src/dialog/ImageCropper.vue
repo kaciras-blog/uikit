3f83a306a4d613c0a39e7d92aad0935a6f074ffd
Kaciras <Kaciras@protonmail.com>
2022-04-14 15:03:47 +0800

    (WIP) ImageCropper: add loading status

--- src/dialog/ImageCropper.vue
+++ src/dialog/ImageCropper.vue
@@ -13,34 +13,58 @@
 		</header>
 
 		<div
-			:class="$style.main"
+			v-show="loadStatus"
+			:class="$style.cropView"
 			@mousedown="drag"
 			@touchstart.self.prevent="drag"
 			@wheel="handleWheel"
 		>
-			<div :style="wrapStyle" ref="wrapper">
+			<div :style="wrapStyle" ref="main">
 				<img
 					:src="image"
 					alt="Image to crop"
 					:class="$style.image"
 					:style="imgStyle"
+					@load="handleLoad"
+					@error="loadStatus = null"
 				>
 			</div>
-			<div :class="$style.region"></div>
+			<div
+				:class="$style.region"
+				:style="stencilStyle"
+			/>
+		</div>
+
+		<!--
+			要放在后面，以便在未加载时覆盖 main 引用。
+		-->
+		<div
+			v-if="loadStatus === false"
+			ref="main"
+			:class="$style.loading"
+		>
+			<AtomSpinner/>
+			加载中...
+		</div>
+		<div
+			v-else-if="loadStatus === null"
+			:class="$style.loading"
+		>
+			加载失败，请重新选择图片以重试。
 		</div>
 
 		<div :class="$style.toolbar">
 			<KxButton
 				type="icon"
 				title="水平翻转"
-				@click="filps.x *= -1"
+				@click="flip.x *= -1"
 			>
 				<SwapHorizIcon/>
 			</KxButton>
 			<KxButton
 				type="icon"
 				title="垂直翻转"
-				@click="filps.y *= -1"
+				@click="flip.y *= -1"
 			>
 				<SwapVertIcon/>
 			</KxButton>
@@ -58,6 +82,14 @@
 			>
 				<ZoomOutMap/>
 			</KxButton>
+
+			<input
+				type="number"
+				title="缩放"
+				min="0.01"
+				step="0.1"
+				v-model="transform.scale"
+			>
 
 			<div :class="$style.stats">
 				{{ region.left }} - {{ region.top }} |
@@ -81,18 +113,28 @@ import SwapVertIcon from "../assets/swap_vert.svg?sfc";
 import RotateRightIcon from "../assets/rotate_right.svg?sfc";
 import ZoomOutMap from "../assets/zoom_out_map.svg?sfc";
 import { useDialog } from "./quick-alert";
-import KxButton from "../input/KxButton.vue";
 import { cursorPosition, observeMouseMove } from "../dragging";
+import KxButton from "../input/KxButton.vue";
+import AtomSpinner from "../components/AtomSpinner.vue";
 
 const props = defineProps({
+	/**
+	 * 要裁剪的图片的 URL。
+	 */
 	image: {
 		type: String,
 		required: true,
 	},
-	mimeType: {
-		type: String,
-		required: true,
+	/**
+	 * 是否使用圆形裁剪框。
+	 */
+	circle: {
+		type: Boolean,
+		default: false,
 	},
+	/**
+	 * 目标宽高比，比如 1 或 16 / 9。
+	 */
 	aspectRatio: {
 		type: Number,
 		required: true,
@@ -100,10 +142,13 @@ const props = defineProps({
 });
 
 const $dialog = useDialog();
+usePreventScroll();
 
-const wrapper = shallowRef<HTMLElement>();
+const main = shallowRef<HTMLElement>();
+const loadStatus = shallowRef<boolean | null>(false);
+const stencilStyle = shallowRef();
 
-const filps = reactive({
+const flip = reactive({
 	x: 1,
 	y: 1,
 });
@@ -116,7 +161,7 @@ const transform = reactive({
 });
 
 const imgStyle = computed(() => ({
-	transform: `scaleX(${filps.x}) scaleY(${filps.y})`,
+	transform: `scaleX(${flip.x}) scaleY(${flip.y})`,
 }));
 
 const wrapStyle = computed(() => {
@@ -133,7 +178,30 @@ const region = computed(() => {
 	};
 });
 
-usePreventScroll();
+function handleLoad(event: Event) {
+	const { width: w, height: h } = (event.target as HTMLImageElement);
+	const { width: vw, height: vh } = main.value!.getBoundingClientRect();
+
+	transform.scale = Math.max(vw / w, vh / h);
+	loadStatus.value = true;
+
+	const s = Math.min(vw / (1/ props.aspectRatio), vh / props.aspectRatio);
+	stencilStyle.value = {
+		top: `calc(50% - ${vw * s / 2}px)`,
+		left: `calc(50% - ${vh * s / 2}px)`,
+		width: `${vw * s}px`,
+		height: `${vh * s}px`,
+	};
+}
+
+function ok() {
+	$dialog.confirm({
+		...region.value,
+		scale: transform.scale,
+		flipHorizontal: flip.x === -1,
+		flipVertically: flip.y === -1,
+	});
+}
 
 function drag(event: MouseEvent | TouchEvent) {
 	const { clientX, clientY } = cursorPosition(event);
@@ -149,16 +217,14 @@ function drag(event: MouseEvent | TouchEvent) {
 
 function handleWheel(event: WheelEvent) {
 	const { deltaY, clientX, clientY } = event;
-	const { width, height } = wrapper.value!.getBoundingClientRect();
+	event.preventDefault();
 
 	const d = 1 - deltaY / 100;
 	transform.scale *= d;
-	transform.x += ((transform.x + width/ 2)  - clientX) * (d - 1);
-	transform.y += ((transform.y + height/ 2) - clientY) * (d - 1);
-}
 
-function ok() {
-	$dialog.confirm(region.value);
+	const { width, height } = main.value!.getBoundingClientRect();
+	transform.x += ((transform.x + width / 2) - clientX) * (d - 1);
+	transform.y += ((transform.y + height / 2) - clientY) * (d - 1);
 }
 </script>
 
@@ -190,9 +256,22 @@ function ok() {
 	justify-content: center;
 	align-items: center;
 	flex: 1;
+}
+
+.cropView {
+	composes: main;
+
+	cursor: move;
 	position: relative;
 	overflow: hidden;
-	cursor: move;
+}
+
+.loading {
+	composes: main;
+
+	flex-direction: column;
+	gap: 10px;
+	font-size: 1.5em;
 }
 
 .image {
@@ -201,13 +280,8 @@ function ok() {
 
 .region {
 	position: absolute;
-	top: 50%;
-	left: 50%;
-	width: 400px;
-	height: 300px;
-
+	border: solid 1px white;
 	pointer-events: none;
-	border: solid 1px black;
 
 	/* 用阴影实现元素外变暗 */
 	box-shadow: 0 0 0 100vmax rgba(0, 0, 0, 0.3);
