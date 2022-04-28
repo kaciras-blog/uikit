e2985c8986d2dba0447f646ca258e155616d5891
Kaciras <Kaciras@protonmail.com>
2022-04-28 15:42:13 +0800

    fix the story of ImageCropper

--- stories/dialog/ImageCropper.stories.ts
+++ stories/dialog/ImageCropper.stories.ts
@@ -10,10 +10,11 @@ export const ImageCropper: Story = () => ({
 	template: `
 		<KxButton @click="showCropper"> 裁剪图片</KxButton>
 		<DialogContainer/>
-		<img v-if="result" :src="result" alt="Cropped image">
+		<img v-if="result" :src="result" alt="Cropped image" :style="style">
 	`,
 	data: () => ({
 		result: null,
+		style: {},
 	}),
 	methods: {
 		async showCropper() {
@@ -21,13 +22,22 @@ export const ImageCropper: Story = () => ({
 
 			const cropper = this.$dialog.show(KxImageCropper, {
 				mimeType: file.type,
-				aspectRatio: 10 / 9,
+				aspectRatio: 16 / 9,
 				image: await blobToURL(file),
 			});
-			const blob = await cropper.confirmPromise;
+			const i = await cropper.confirmPromise;
+
+			this.style = {
+				display: "block",
+				marginTop: "20px",
+				objectPosition: `${-i.left}px ${-i.top}px`,
+				objectFit: "none",
+				width: `${i.width}px`,
+				height: `${i.height}px`,
+			};
 
 			URL.revokeObjectURL(this.result);
-			this.result = URL.createObjectURL(blob);
+			this.result = URL.createObjectURL(file);
 		},
 	},
 });
