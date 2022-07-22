565909125f0ea963ec8e0e6349c49d39ebdb91dc
Kaciras <Kaciras@protonmail.com>
2022-07-22 11:05:54 +0800

    ImageCropper: fix dialog result

--- stories/dialog/ImageCropper.stories.ts
+++ stories/dialog/ImageCropper.stories.ts
@@ -8,50 +8,61 @@ export default {
 
 export const ImageCropper: Story = () => ({
 	template: `
-		<KxButton
-			@click="showCropper"
-		>
-			裁剪图片
-		</KxButton>
-		<DialogContainer/>
-		<img
-			v-if="result"
-			class="cropped"
-			:src="result"
-			alt="裁剪结果"
-			:style="style"
-		>
+		<div class="crop-image">
+			<KxButton @click="showCropper">
+				裁剪图片
+			</KxButton>
+			<pre>
+				{{ JSON.stringify(result, null, 4) }}
+			</pre>
+			<img
+				v-if="imageURL"
+				class="cropped"
+				:src="imageURL"
+				alt="裁剪结果"
+				:style="style"
+			>
+		</div>
+		<DialogContainer></DialogContainer>
 	`,
 	data: () => ({
-		result: null,
-		style: {},
+		imageURL: null,
+		result: undefined,
 	}),
+	computed: {
+		style() {
+			const crop = this.result;
+			if (!crop) {
+				return {};
+			}
+
+			let transform = `rotate(${crop.rotate}deg)`;
+			if (crop.flipX) {
+				transform += " scaleX(-1)";
+			}
+			if (crop.flipY) {
+				transform += " scaleY(-1)";
+			}
+
+			return {
+				width: `${crop.width}px`,
+				height: `${crop.height}px`,
+				transform,
+				objectPosition: `${-crop.left}px ${-crop.top}px`,
+			};
+		},
+	},
 	methods: {
 		async showCropper() {
 			const image = await openFile("image/*");
-
 			const cropper = this.$dialog.show(ImageCropperVue, {
 				image,
 				aspectRatio: 16 / 9,
 			});
-			const i = await cropper.confirmPromise;
 
-			this.style = {
-				transform: `rotate(${i.rotate}deg)`,
-				objectPosition: `${-i.left}px ${-i.top}px`,
-				width: `${i.width}px`,
-				height: `${i.height}px`,
-			};
-
-			if (i.flipX) {
-				this.style.transform += " scaleX(-1)";
-			}
-			if (i.flipY) {
-				this.style.transform += " scaleY(-1)";
-			}
-
-			URL.revokeObjectURL(this.result);
-			this.result = URL.createObjectURL(image);
+			this.result = await cropper.confirmPromise;
+			URL.revokeObjectURL(this.imageURL);
+			this.imageURL = URL.createObjectURL(image);
 		},
 	},
 });
