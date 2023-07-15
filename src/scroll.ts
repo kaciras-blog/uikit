fe0e40c08955861889df1bf852ce5dc98682bd24
Kaciras <Kaciras@protonmail.com>
2023-07-15 21:14:50 +0800

    update deps

--- src/scroll.ts
+++ src/scroll.ts
@@ -1,4 +1,20 @@
 /**
+ *
+ * @param el 要检查的元素
+ * @param dy 元素的边界在 Y 轴向上偏移
+ * @param dx 元素的边界在 X 轴向左偏移
+ */
+export function isInViewport(el: Element, dy = 0, dx = 0) {
+	const rect = el.getBoundingClientRect();
+	return (
+		rect.right > dx &&
+		rect.bottom > dy &&
+		rect.x < window.innerWidth + dx &&
+		rect.y < window.innerHeight + dy
+	);
+}
+
+/**
  * 获取文档当前的的滚动高度，兼容各种浏览器。
  *
  * @return 滚动高度
@@ -7,42 +23,4 @@
 export function getScrollTop() {
 	const doc = document.documentElement || document.body.parentNode;
 	return (typeof doc.scrollTop === "number" ? doc : document.body).scrollTop;
-}
-
-/**
- * 将多个元素的垂直滚动条按百分比同步。
- *
- * 该函调用时会立即进行一次同步，以第一个参数的元素为基准。
- *
- * @param elements 要同步的元素
- * @return 取消同步的函数
- */
-export function syncScroll(...elements: HTMLElement[]) {
-	let skip = false;
-
-	if (elements.length) {
-		sync(elements[0]);
-	}
-
-	function sync(target: HTMLElement) {
-		const p = target.scrollTop / (target.scrollHeight - target.offsetHeight);
-		elements.forEach(el => el.scrollTop = p * (el.scrollHeight - el.offsetHeight));
-	}
-
-	function scrollHandler(event: Event) {
-		if (skip) {
-			return;
-		}
-		skip = true;
-
-		// 必须要延迟到下一帧，否则在开启了平滑滚动的浏览器上会滚不动
-		requestAnimationFrame(() => {
-			sync(event.target as HTMLElement);
-			requestAnimationFrame(() => skip = false);
-		});
-	}
-
-	elements.forEach(el => el.addEventListener("scroll", scrollHandler));
-
-	return () => elements.forEach(el => el.removeEventListener("scroll", scrollHandler));
 }
