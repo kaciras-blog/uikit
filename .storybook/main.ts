d3d68c21967dcd2ba48143abace3a2fc58bbfe32
Kaciras <Kaciras@protonmail.com>
2024-04-25 10:55:39 +0800

    replace `vue-docgen-api` with `vue-component-meta`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,11 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
-import { mergeConfig } from "vite";
-import { join } from "path";
-import { fileURLToPath } from "url";
 
 export default <StorybookConfig>{
 	framework: {
 		name: "@storybook/vue3-vite",
-		options: {},
+		options: {
+			docgen: "vue-component-meta",
+		},
 	},
 	addons: [
 		"@storybook/addon-links",
@@ -17,19 +16,6 @@ export default <StorybookConfig>{
 		autodocs: "tag",
 	},
 	stories: [
-		// "../stories/**/*.mdx",
 		"../stories/**/*.stories.ts",
 	],
-	viteFinal(config) {
-		const vueDocgenIndex = config.plugins.findIndex(({ name }) => name === 'storybook:vue-docgen-plugin')
-		if (vueDocgenIndex !== -1) config.plugins.splice(vueDocgenIndex, 1)
-
-		return mergeConfig(config, {
-			resolve: {
-				alias: {
-					"@": join(fileURLToPath(import.meta.url), "../../src")
-				}
-			},
-		})
-	}
 };
